0faceba41e23549f698844c996bb5b5e2ee6ccbb
Omar Askr <90140084+omaraskr1@users.noreply.github.com>
2022-02-19 20:32:49 +0200

    Initial commit

---
+++ clint/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace clint
+{
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			
+
+
+
+
+
+			IPAddress host = IPAddress.Parse("127.0.0.1");
+			
+			IPEndPoint hostEndpoint = new IPEndPoint(host, 8000);
+			
+
+			Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			clientSock.Connect(hostEndpoint);
+		
+			
+				Console.WriteLine("getting file....");
+				byte[] clientData = new byte[1024];
+				int receivedBytesLen = clientSock.Receive(clientData);
+				int fileNameLen = BitConverter.ToInt32(clientData, 0);
+				string fileName = Encoding.ASCII.GetString(clientData, 4, fileNameLen);
+				BinaryWriter bWrite = new BinaryWriter(File.Open(fileName, FileMode.Create));
+				bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+				bWrite.Close();
+				
+			
+
+			
+			Console.WriteLine("Conversation Ended");
+			clientSock.Shutdown(SocketShutdown.Both);
+			clientSock.Close();
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server
+{
+	class Program
+	{
+		
+		static void Main(string[] args)
+		{
+			
+
+			
+
+
+
+
+			IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+			
+
+			Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			sock.Bind(ipEnd);
+
+			sock.Listen(100);
+			Socket clientSock = sock.Accept();
+		
+			
+			string fileName = "file.txt";// "c:\\filetosend.txt";
+			byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+			byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+			byte[] fileData = File.ReadAllBytes(fileName);
+			byte[] serverData = new byte[4 + fileNameByte.Length + fileData.Length];
+			fileNameLen.CopyTo(serverData, 0);
+			fileNameByte.CopyTo(serverData, 4);
+			fileData.CopyTo(serverData, 4 + fileNameByte.Length);
+			clientSock.Send(serverData);
+			
+			clientSock.Shutdown(SocketShutdown.Both);
+			clientSock.Close();
+			Console.WriteLine(fileName);
+			Console.WriteLine("omar khaled nasr askr");
+			Console.WriteLine("is");
+			Console.WriteLine("section 6");
+			Console.WriteLine("listening at ip: " +ipEnd.Address +" ,port: "+ipEnd.Port);
+			Console.WriteLine("clinet accepted");
+			Console.WriteLine("sending file: "+fileName);
+			Console.WriteLine("file sent ");
+
+
+		}
+	}
+}
